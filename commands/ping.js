a1a2088a5d8604e9d36321232ab58398ed861b8d
beq3 <47683029+beq3@users.noreply.github.com>
2019-02-16 13:57:22 +0000

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     var ms = await message.channel.send("Ping?")
     var botms = ms.createdTimestamp - message.createdTimestamp;
-    ms.edit('A Latência é ' + botms + ' ms.  A Latencia da API é ' + Math.round(bot.ping)+ 'ms.W');
+    ms.edit('A Latência é ' + botms + ' ms.  A Latencia da API é ' + Math.round(bot.ping)+ 'ms.');
 
 }
 
